e56df9be790699f433d850d74fb12d3a0ab5f291
Agata Kubiak-Kokot <kubiak.aga@gmail.com>
2020-05-01 11:08:11 +0200

    Styling of list elements (<li>) when checked

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
+// ELELENTS FROM DOM
 const input = document.querySelector('.form__item-input');
 const quantity = document.querySelector('.form__item-quantity')
 const addBtn = document.querySelector('.form__submit-btn');
 const list = document.querySelector('.form__list-items');
 const formContainer = document.querySelector('.form__alert-messages-container');
-console.log(formContainer);
 let alertItem;
 
+// FUNCTIONS
 function removeAlertItems(){
     if(alertItem){
         let alertAll = document.querySelectorAll('.alert');
@@ -14,33 +15,41 @@ function removeAlertItems(){
         });
     }  
 }
-
-function checkedItemsAddStyle() {
-
-}
-
 function addItems(e) {
     e.preventDefault() 
     if(input.value!=""&&quantity.value!=""){
+        // deleting alerts on input
         input.classList.remove('input-alert');
         quantity.classList.remove('input-alert');
+        //creating the <li> elements
         let listItem = input.value;
         let itemQuantity = quantity.value;
         const li = document.createElement('li');
         li.className = 'form__list-item';
         li.draggable = 'true';
-        // console.log('ok', listItem);
+        //creating input type=checkbox
         const checkBox = document.createElement('input');
         checkBox.type = 'checkbox';
         checkBox.id = listItem;
+        //creating label for input type=checkbox
         const checkBoxLabel = document.createElement('label');
         checkBoxLabel.htmlFor = 'id';
         checkBoxLabel.textContent = `${listItem} [${itemQuantity}]`;
+        //appending elements to list
         list.appendChild(li);
         li.appendChild(checkBox);
         li.appendChild(checkBoxLabel);
         input.value='';
         quantity.value = '';
+        //styling of <li> :checked
+        checkBox.addEventListener('change', (e)=>{
+            if(e.target.checked){
+                console.log('działa');
+                li.classList.add('li-checked');
+            }else{
+                li.classList.remove('li-checked');
+            }
+        })
         removeAlertItems()
     }else{
         alertItem = document.createElement('p');
@@ -68,6 +77,5 @@ function addItems(e) {
     }
    
 }
-
-
+// LISTENERS
  addBtn.addEventListener('click', addItems);
