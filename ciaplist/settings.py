e088ed243b9a8e6e64fbad76ce458f482f6b3fb3
Maciek Kokot <maciej.kokot@gmail.com>
2020-05-05 22:35:40 +0200

    błąd 500: wyłączenie kompresji

--- ciaplist/settings.py
+++ ciaplist/settings.py
@@ -125,7 +125,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 try:
     from ciaplist.secrets import *
