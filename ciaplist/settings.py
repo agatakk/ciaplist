404ef5120e6be3689ab45d2756939d673066f36e
Maciek Kokot <maciej.kokot@gmail.com>
2020-05-05 22:22:22 +0200

    heroku static adjustments

--- ciaplist/settings.py
+++ ciaplist/settings.py
@@ -122,13 +122,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-django_heroku.settings(locals())
+
 try:
     from ciaplist.secrets import *
 except ImportError:
     pass
+
+django_heroku.settings(locals())
