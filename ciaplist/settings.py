f0721a7eb1237787e79fed2d444399698897f7f9
Maciek Kokot <maciej.kokot@gmail.com>
2020-05-05 22:51:44 +0200

    heroku-działająca konfiguracja static

--- ciaplist/settings.py
+++ ciaplist/settings.py
@@ -122,10 +122,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 try:
     from ciaplist.secrets import *
