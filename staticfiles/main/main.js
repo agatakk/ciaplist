363f1f8ebe5fae06d13cd258cb8e94f69038c911
Maciek Kokot <maciej.kokot@gmail.com>
2020-06-29 20:46:25 +0200

    stupid collectstatic because heroku

--- staticfiles/main/main.js
+++ staticfiles/main/main.js
@@ -18,12 +18,11 @@ function removeAlertItems(){
 }
 function addItems(e) {
     // e.preventDefault();
-    
     if(input.value.trim()!=""&&quantity.value.trim()!=""){
         // deleting alerts on input
         input.classList.remove('input-alert');
         quantity.classList.remove('input-alert');
-        //creating the <li> elements
+        // creating the <li> elements
         // let listItem = input.value;
         // let itemQuantity = quantity.value;
         // const li = document.createElement('li');
@@ -44,77 +43,24 @@ function addItems(e) {
         // li.appendChild(checkBox);
         // li.appendChild(checkBoxLabel);
         
-        //dragdrop
-        const del = document.querySelector('#del');
-        function dragStart (e) {
-            if(e.target&&e.target.nodeName == "LI"){
-                console.log('start');
-                // del.textContent = 'X';
-                del.className = "del";
-                setTimeout(()=>e.target.className = 'none', 0);                
-                e.dataTransfer.setData('Text', e.target.id);
-            }
-            
-        }
-        function dragEnd(e){
-            console.log('koniec')
-            if( e.target&&e.target.nodeName == "LI"){
-                del.className = "none";
-                e.target.className = "form__list-item"; 
-                console.log('end');
-            }
-        }
-        function dragEnter (e) {
-            if(e.target&&e.target.className==='del'){
-                console.log('enter');
-                e.target.classList.add('del-enter');
-            }
-        }
-        function dragOver (e) {
-            if(e.target&&e.target.matches('div.del')){
-                e.preventDefault();
-                console.log('over')
-            }
-        }
-        //drop
-        function dropElement (e) {
-            if(e.target&&e.target.matches('div.del')){
-                // e.preventDefault();
-                console.log('drop');
-                const data = e.dataTransfer.getData("Text");
-                const element = document.getElementById(data);
-                element.remove();
-                del.className = 'none';
-            }
-        };
-        // const label = document.querySelectorAll('li>label');
-        // console.log(label);
-        // label.forEach((item)=>{
-        //     const listItems = document.querySelector(`li[data-key="${item.htmlFor}"]`);
-        //     console.log(listItems);    
-        //     
-        // });
-        document.addEventListener('dragstart', dragStart);
-        document.addEventListener('dragend', dragEnd);
-        document.addEventListener('dragenter', dragEnter);
-        document.addEventListener('dragover', dragOver);
-        window.addEventListener('drop', dropElement);
+        
         // input.value='';
         // quantity.value = '';
 
-        //styling of <li> :checked
-        checkBox.addEventListener('change', (e)=>{
-            if(e.target.checked){
-                console.log('działa');
-                li.classList.add('li-checked');
-            }else{
-                li.classList.remove('li-checked');
-            }
-        })
+        // //styling of <li> :checked
+        // checkBox.addEventListener('change', (e)=>{
+        //     if(e.target.checked){
+        //         console.log('działa');
+        //         li.classList.add('li-checked');
+        //     }else{
+        //         li.classList.remove('li-checked');
+        //     }
+        // })
 
         //removing alert items
         removeAlertItems()
     }else{
+        e.preventDefault();
         alertItem = document.createElement('p');
         alertItem.className = 'alert';
         //NO ITEM
@@ -140,5 +86,98 @@ function addItems(e) {
     }
   
 }
-// LISTENERS
+// LISTENERS for addItems funntion
  form.addEventListener('submit', addItems);
+//-------------------------------------
+ //dragdrop
+ const del = document.querySelector('#del');
+ const trash = document.querySelector('.trash');
+ function dragStart (e) {
+    
+     if(e.target&&e.target.nodeName == "LI"){
+         console.log('start');
+         del.className = "del";
+         e.dataTransfer.setData('Text', e.target.id);
+         setTimeout(()=>{e.target.className = 'none'}, 0);                
+     }
+     
+ }
+ function dragEnd(e){
+     console.log('koniec')
+     if( e.target&&e.target.nodeName == "LI"){
+         del.className = "none";
+         e.target.className = "form__list-item"; 
+         console.log('end');
+     }
+ }
+ function dragEnter (e) {
+     if(e.target&&(e.target.matches('img.trash')||e.target.className==='del')){
+         console.log('enter');
+        del.classList.add('del-enter');
+        trash.classList.add('del-enter');
+        //  e.target.classList.add('del-enter');
+     }
+ }
+ function dragOver (e) {
+     if(e.target&&(e.target.matches('img.trash')||e.target.matches('div.del'))){
+         e.preventDefault();
+         console.log('over')
+     }
+ }
+ function dragLeave(e){
+    if(e.target&&e.target.matches('img.trash')){
+        console.log('leave');
+        del.classList.remove('del-enter');
+        trash.classList.remove('del-enter');
+    }
+ }
+ //drop
+ function dropElement (e) {
+     if(e.target&&(e.target.matches('img.trash')||e.target.matches('div.del'))){
+         e.preventDefault();
+         console.log('drop');
+         const data = e.dataTransfer.getData("Text");
+         const element = document.getElementById(data);
+         element.remove();
+         del.className = 'none';
+     }
+ };
+
+
+ //listiners for drag-drop
+ document.addEventListener('dragstart', dragStart);
+ document.addEventListener('dragend', dragEnd);
+ document.addEventListener('dragenter', dragEnter);
+ document.addEventListener('dragover', dragOver);
+ document.addEventListener('dragleave', dragLeave);
+ document.addEventListener('drop', dropElement);
+
+ //changing a whole <li> tag when its checkbox is chekcked
+
+ function checkedItem(e){
+     if(e.target&&(e.target.nodeName=='INPUT'||e.target.nodeName=='LABEL')){
+         const listItem = document.querySelector(`li[data-key="${e.target.id}"]`);
+         if(e.target.checked){
+             listItem.classList.add('li-checked')
+         }else{
+            listItem.classList.remove('li-checked')
+         }
+        //  console.log(e.target.htmlFor);
+        //  console.log(listItem)
+        //  console.log(e)
+     }
+    // if(e.target.checked){
+    //     console.log('działa')
+        
+    // }else{
+    //     console.log('nie działa')
+    // }
+//     const checkBoxes = document.querySelectorAll('li>input');
+//     checkBoxes.forEach((item)=>{
+//      console.log(listItems);    
+     
+//  });
+}
+
+
+ window.addEventListener('change', checkedItem);
