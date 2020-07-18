61094b045014e3e2ba7eecab946dd9992d07a481
Maciek Kokot <maciej.kokot@gmail.com>
2020-07-18 23:08:29 +0200

    enable password reset

--- ciaplist/settings.py
+++ ciaplist/settings.py
@@ -132,6 +132,17 @@ STATICFILES_DIRS = (
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+try:
+    EMAIL_HOST = os.environ['EMAIL_HOST']
+    EMAIL_PORT = os.environ['EMAIL_PORT']
+    EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+    EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+    EMAIL_USE_TLS = True
+    DEFAULT_FROM_EMAIL = os.environ['DEFAULT_FROM_EMAIL']
+except KeyError:
+    pass
+
 try:
     from ciaplist.secrets import *
 except ImportError:
